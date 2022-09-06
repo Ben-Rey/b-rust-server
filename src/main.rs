4885028b95a0657bcb1f98c5ee11351a5250e4b5
fen|x <benjamin.reynes.dev@gmail.com>
2022-09-06 15:59:31 +0200

    modules

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use server::Server;
+use http::Method;
+
+mod http;
+mod server;
+
 fn main() {
     let get = Method::GET;
     let put = Method::PUT;
@@ -5,40 +11,6 @@ fn main() {
     let delete = Method::DELETE;
 
     let server = Server::new("127.0.0.1:8080".to_string());
-    server.run()
-}
+    server.run();
 
-struct Server {
-    addr: String, 
-}
-
-impl Server {
-    fn new(addr: String) -> Self {
-        Self {
-            addr
-        }
-    }
-    
-    fn run(self) {
-        println!("Listening on : {}", self.addr)
-    }
-}
-
-
-struct Request {
-    path: String,
-    query_string: Option<String>,
-    method: Method,
-}
-
-enum Method {
-    GET,
-    POST,
-    PUT,
-    DELETE,
-    HEAD, 
-    CONNECT,
-    OPTIONS,
-    TRACE,
-    PATCH 
 }
